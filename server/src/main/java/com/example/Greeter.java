cc5f78de99de666156ab8c894712a2b7c283a9a7
guinokali <cesar.rodriguezgo@cun.edu.co>
2020-01-29 05:09:52 -0500

    otros checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,13 @@ public class Greeter {
 
   }
 
+  /**
+  	* @param someone the name of a person
+  	* @return greeting string
+  	*/
 
-  public String greet(final String someone) {
+
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
